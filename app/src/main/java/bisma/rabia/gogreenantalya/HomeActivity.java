612a16b977ab1a75215fb24d3196aca5422de6f7
rabismail <rabismail@hotech.systems>
2019-12-16 23:35:32 +0300

    added bottom navigation bar, added three items for now.

--- app/src/main/java/bisma/rabia/gogreenantalya/HomeActivity.java
+++ app/src/main/java/bisma/rabia/gogreenantalya/HomeActivity.java
@@ -2,9 +2,14 @@ package bisma.rabia.gogreenantalya;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import bisma.rabia.gogreenantalya.databinding.LayoutActivityHomeBinding;
 
@@ -13,7 +18,31 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        LayoutActivityHomeBinding layoutActivityHomeBinding = DataBindingUtil.inflate(LayoutInflater.from(this), R.layout.layout_activity_home, null, false);
+        final LayoutActivityHomeBinding layoutActivityHomeBinding = DataBindingUtil.inflate(LayoutInflater.from(this), R.layout.layout_activity_home, null, false);
         setContentView(layoutActivityHomeBinding.getRoot());
+
+        layoutActivityHomeBinding.bnvActivityHome.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.mni_bottom_nav_home:
+                        layoutActivityHomeBinding.lyoHome.setVisibility(View.VISIBLE);
+                        layoutActivityHomeBinding.lyoStoresName.setVisibility(View.GONE);
+                        layoutActivityHomeBinding.lyoAboutus.setVisibility(View.GONE);
+                        break;
+                    case R.id.mni_bottom_nav_partners:
+                        layoutActivityHomeBinding.lyoHome.setVisibility(View.GONE);
+                        layoutActivityHomeBinding.lyoStoresName.setVisibility(View.VISIBLE);
+                        layoutActivityHomeBinding.lyoAboutus.setVisibility(View.GONE);
+                        break;
+                    case R.id.mni_bottom_nav_about:
+                        layoutActivityHomeBinding.lyoHome.setVisibility(View.GONE);
+                        layoutActivityHomeBinding.lyoStoresName.setVisibility(View.GONE);
+                        layoutActivityHomeBinding.lyoAboutus.setVisibility(View.VISIBLE);
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
