7d910797bd4d152c80a9a5a6fde4c1ee6815815f
rabismail <rabismail@hotech.systems>
2019-12-17 23:16:48 +0300

    designed home fragment and retrieved balance from the server.

--- app/src/main/java/bisma/rabia/gogreenantalya/fragment/HomeFragment.java
+++ app/src/main/java/bisma/rabia/gogreenantalya/fragment/HomeFragment.java
@@ -7,15 +7,38 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import bisma.rabia.gogreenantalya.R;
+import bisma.rabia.gogreenantalya.databinding.FragmentHomeBinding;
+import bisma.rabia.gogreenantalya.utils.CheckBalanceTask;
 
 public class HomeFragment extends Fragment {
+
+    FragmentHomeBinding fragmentHomeBinding;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        fragmentHomeBinding = DataBindingUtil.inflate(LayoutInflater.from(getActivity()), R.layout.fragment_home, container, false);
+        return fragmentHomeBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        String nodeUrl = getString(R.string.node_url);
+        new CheckBalanceTask(new CheckBalanceTask.CheckBalanceTaskCallback() {
+            @Override
+            public void update(String updateContent) {
+
+            }
+
+            @Override
+            public void finish(boolean success, String updateContent, String balance) {
+                fragmentHomeBinding.txvHomeBalance.setText("Point \n" + balance);
+            }
+        }).execute(nodeUrl, "rabia");
     }
 }
